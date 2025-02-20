e95ca32b5cab6a7253eaeb88cff44d657456129e
Victor Amelinckx <68256527+victoramelinckx@users.noreply.github.com>
2025-02-20 14:04:52 -0500

    Initial commit from WebGo

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+        return (
+          <main className="min-h-screen flex flex-col items-center justify-center p-24 text-center">
+            <h1 className="text-6xl font-bold text-primary mb-6">
+              Impulsa Tus Ventas Como Nunca Antes Con Webgo
+            </h1>
+            <p className="text-2xl text-secondary max-w-2xl">
+              Creamos y Mejoramos Tu Sitio Web de Manera Profesional y Gratuita. Nuestro Enfoque Principal: Incrementar Tus Ventas
+            </p>
+          </main>
+        )
+      }
